00ea8bf54fc6d67a3f136356e0bc168bc5cefe5e
filipkor <filipkor@student.chalmers.se>
2026-02-12 19:01:47 +0100

    init

--- src/CarController.java
+++ src/CarController.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 /*
 * This class represents the Controller part in the MVC pattern.
